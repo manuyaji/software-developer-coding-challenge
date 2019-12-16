6a67389629f0dd263c9f6d86f4eba83533524cdc
Manjunath Yaji <manuyaji@gmail.com>
2019-12-17 02:02:09 +0530

    Formatted source code

--- src/main/java/com/yaji/traderev/carauction/services/auctions/ICarAuctionService.java
+++ src/main/java/com/yaji/traderev/carauction/services/auctions/ICarAuctionService.java
@@ -1,7 +1,5 @@
 package com.yaji.traderev.carauction.services.auctions;
 
-import java.util.List;
-
 import com.yaji.traderev.carauction.entity.CarAuction;
 import com.yaji.traderev.carauction.entity.CarBid;
 import com.yaji.traderev.carauction.entity.User;
@@ -10,6 +8,7 @@ import com.yaji.traderev.carauction.exception.TradeRevInvalidInputException;
 import com.yaji.traderev.carauction.exception.TradeRevResourceNotFoundException;
 import com.yaji.traderev.carauction.models.requestdto.CarAuctionRequestDto;
 import com.yaji.traderev.carauction.services.IResourceService;
+import java.util.List;
 
 public interface ICarAuctionService extends IResourceService<CarAuction, CarAuctionRequestDto> {
 
@@ -19,6 +18,8 @@ public interface ICarAuctionService extends IResourceService<CarAuction, CarAuct
       throws TradeRevInvalidInputException, TradeRevResourceNotFoundException;
 
   public CarAuction closeAuction(Integer auctionId) throws TradeRevInvalidInputException;
-  
-  public List<CarBid> getHistory(Integer auctionId, Integer page, Integer size, ResourceSortingOrder order) throws TradeRevInvalidInputException;
+
+  public List<CarBid> getHistory(
+      Integer auctionId, Integer page, Integer size, ResourceSortingOrder order)
+      throws TradeRevInvalidInputException;
 }
